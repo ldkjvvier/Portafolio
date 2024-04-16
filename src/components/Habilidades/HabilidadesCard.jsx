c6682cc0ae7d759acf0c3d8a64e6a306f78cd92d
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-04-16 17:23:48 -0400

    Refactor HabilidadesCard component to improve layout and styling

--- src/components/Habilidades/HabilidadesCard.jsx
+++ src/components/Habilidades/HabilidadesCard.jsx
@@ -2,17 +2,17 @@ import PropTypes from 'prop-types';
 
 export const HabilidadesCard = ({ icon, title, color }) => {
   return (
-    <div>
-      <div
-        className={`${color} bg-opacity-20 dark:bg-opacity-30 rounded-lg p-3 cursor-pointer`}
-      >
-        {icon}
-      </div>
-      <h3 className="block text-sm text-center leading-8 font-bold text-black dark:text-cyan-50 mb-3">
-        {title}
-      </h3>
-    </div>
-  )
+		<div className="w-auto">
+			<div
+				className={`${color} bg-opacity-20 w-[72px] dark:bg-opacity-30 rounded-lg p-3 cursor-pointer`}
+			>
+				{icon}
+			</div>
+			<h3 className="block text-sm text-center leading-8 font-bold text-black dark:text-cyan-50 mb-3">
+				{title}
+			</h3>
+		</div>
+	)
 }
 
 HabilidadesCard.propTypes = {
