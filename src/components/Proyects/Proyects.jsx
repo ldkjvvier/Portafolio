c1faa202c218417164e63fc55551b3f5620d56a2
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-04-17 18:30:09 -0400

    Fix typo in Proyects.jsx project description

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -2,7 +2,7 @@ import { ProyectCard } from './ProyectCard'
 
 export const Proyects = () => {
 	return (
-		<div id='proyectos' className=' scroll-m-32'>
+		<div id="proyectos" className=" scroll-m-32">
 			<div className="my-20 border-b border-gray-400">
 				<h2 className="text-3xl font-bold tracking-tight text-slate-800 dark:text-white sm:text-4xl mb-3">
 					Proyectos
@@ -14,7 +14,7 @@ export const Proyects = () => {
 					description="Proyecto de juego de gato desarrollado en ReactJS, con
 					funcionalidad de juego contra la computadora."
 					img={'Images/TicTacToe.png'}
-					sitioWeb={''}
+					sitioWeb={'https://ldkjvvier.github.io/Tic-Tac-Toe/'}
 					codigo={'https://github.com/ldkjvvier/Tic-Tac-Toe'}
 				/>
 				<ProyectCard
