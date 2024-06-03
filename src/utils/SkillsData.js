7d869d5db5a894fda8755460bfedc0d3ef086a9c
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-03 02:01:19 -0400

    Refactor Skills component to use dynamic data from SkillsData.js for improved maintainability and scalability

---
+++ src/utils/SkillsData.js
@@ -0,0 +1,72 @@
+export const skillsData = [
+	{
+		icon: './habilities/html.png',
+		title: 'HTML5',
+		color: 'bg-orange-500',
+	},
+	{
+		icon: './habilities/css.png',
+		title: 'CSS3',
+		color: 'bg-cyan-500',
+	},
+	{
+		icon: './habilities/js.png',
+		title: 'JavaScript',
+		color: 'bg-yellow-500',
+	},
+	{
+		icon: './habilities/react.png',
+		title: 'React',
+		color: 'bg-blue-500',
+	},
+	{
+		icon: './habilities/nodejs.png',
+		title: 'NodeJS',
+		color: 'bg-green-600',
+	},
+	{
+		icon: './habilities/mongodb.png',
+		title: 'MongoDB',
+		color: 'bg-green-500',
+	},
+	{
+		icon: './habilities/mysql.png',
+		title: 'MySQL',
+		color: 'bg-blue-500',
+	},
+	{
+		icon: './habilities/firebase.png',
+		title: 'Firebase',
+		color: 'bg-yellow-500',
+	},
+	{
+		icon: './habilities/tailwind.png',
+		title: 'TailwindCSS',
+		color: 'bg-cyan-500',
+	},
+	{
+		icon: './habilities/bootstrap.png',
+		title: 'Bootstrap',
+		color: 'bg-purple-700',
+	},
+	{
+		icon: './habilities/redux.png',
+		title: 'Redux',
+		color: 'bg-purple-600',
+	},
+	{
+		icon: './habilities/git.png',
+		title: 'Git',
+		color: 'bg-red-500',
+	},
+  {
+    icon: './habilities/Typescript.png',
+    title: 'TypeScript',
+    color: 'bg-blue-500',
+  },
+  {
+    icon: './habilities/Python.png',
+    title: 'Python',
+    color: 'bg-yellow-500',
+  }
+]
