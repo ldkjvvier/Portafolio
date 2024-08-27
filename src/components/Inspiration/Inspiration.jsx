3c387a8f1c0d6fbbb8039933a4d6ff289ee5f3a6
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-27 01:34:32 -0400

    update inspirations font colors

--- src/components/Inspiration/Inspiration.jsx
+++ src/components/Inspiration/Inspiration.jsx
@@ -5,10 +5,10 @@ export const Inspiration = () => {
 	return (
 		<div className="grid place-items-center border border-slate-600 rounded-lg px-4 py-6 sm:px-8 sm:py-10 my-32 ">
 			<div className="grid grid-rows-2 place-items-center text-center">
-				<h2 className="text-3xl font-bold tracking-tight text-slate-800 dark:text-white sm:text-4xl mb-3">
+				<h2 className="text-3xl font-bold tracking-tight text-slate-800 dark:text-gray-200 sm:text-4xl mb-3">
 					Personas que me inspiran
 				</h2>
-				<p className="z-20 leading-normal text-muted-foreground sm:text-lg sm:leading-7 md:max-w-[65%] text-gray-500">
+				<p className="z-20 leading-normal text-muted-foreground sm:text-lg sm:leading-7 md:max-w-[65%] text-gray-400">
 					Quizás te interesen los siguientes perfiles. Los cuales me
 					han ayudado y motivado a seguir adelante con este mundo de
 					la informatica.
