856f2e4a8cce972419ef0a4e7f4b3a34e37dbda4
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-11-06 00:32:31 -0300

    fix: image display in proyects

--- src/constants/ProyectsData.ts
+++ src/constants/ProyectsData.ts
@@ -4,7 +4,7 @@ export const Proyectos = [
     id: 1,
     description:
       'Juego clásico de Tic Tac Toe desarrollado en ReactJS, donde competir con personas en la misma computadora.',
-    img: 'Portafolio/Images/proyects/TicTacToe.webp',
+    img: '/Portafolio/images/proyects/TicTacToe.webp',
     sitioWeb: 'https://ldkjvvier.github.io/Tic-Tac-Toe/',
     codigo: 'https://github.com/ldkjvvier/Tic-Tac-Toe',
     imgPosicion: ['md:col-start-1', 'md:col-start-2'],
@@ -14,7 +14,7 @@ export const Proyectos = [
     title: 'Pokedex - 2024',
     id: 2,
     description: 'Aplicación de Pokedex en ReactJS que permite buscar pokemones y ver detalles de cada uno.',
-    img: 'Portafolio/Images/proyects/Pokedex.webp',
+    img: '/Portafolio/images/proyects/Pokedex.webp',
     sitioWeb: 'https://pokedex-flame-zeta.vercel.app',
     codigo: 'https://github.com/ldkjvvier/Pokedex',
     imgPosicion: ['md:col-start-2', 'md:col-start-1'],
@@ -25,7 +25,7 @@ export const Proyectos = [
     id: 3,
     description:
       'CineVerse es una aplicación web moderna desarrollada en Next.js que permite a los usuarios buscar y explorar una biblioteca de películas, con acceso a detalles como sinopsis y valoraciones.',
-    img: 'Portafolio/Images/proyects/CineVerse.webp',
+    img: '/Portafolio/images/proyects/CineVerse.webp',
     sitioWeb: 'https://movies-client-next.vercel.app/browse',
     codigo: 'https://github.com/ldkjvvier/Movies-client-next',
     imgPosicion: ['md:col-start-1', 'md:col-start-2'],
