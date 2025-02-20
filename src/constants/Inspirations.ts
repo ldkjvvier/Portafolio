b3f2201145baf7b4e397688279e89f0f7c539e49
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-02-20 20:26:32 -0300

    fix: update image paths to use consistent casing in Inspirations and ProyectsData constants

--- src/constants/Inspirations.ts
+++ src/constants/Inspirations.ts
@@ -3,30 +3,30 @@ export const INSPIRATIONS = [
     id: 1,
     username: 'midudev',
     url: 'https://www.youtube.com/c/midudev',
-    icon: '/Portafolio/images/inspiration/midudev.jpg'
+    icon: '/Portafolio/Images/inspiration/midudev.jpg'
   },
   {
     id: 2,
     username: 'HolaMundo',
     url: 'https://www.youtube.com/@HolaMundoDev',
-    icon: '/Portafolio/images/inspiration/holamundo.jpg'
+    icon: '/Portafolio/Images/inspiration/holamundo.jpg'
   },
   {
     id: 3,
     username: 'MoureDev',
     url: 'https://www.youtube.com/@mouredev',
-    icon: '/Portafolio/images/inspiration/mouredev.jpg'
+    icon: '/Portafolio/Images/inspiration/mouredev.jpg'
   },
   {
     id: 4,
     username: 'DeivChoi',
     url: 'https://www.youtube.com/@DeivChoi',
-    icon: '/Portafolio/images/inspiration/deivchoi.jpg'
+    icon: '/Portafolio/Images/inspiration/deivchoi.jpg'
   },
   {
     id: 5,
     username: 'Fazt',
     url: 'https://www.youtube.com/@FaztTech',
-    icon: '/Portafolio/images/inspiration/fazt.jpg'
+    icon: '/Portafolio/Images/inspiration/fazt.jpg'
   }
 ];
