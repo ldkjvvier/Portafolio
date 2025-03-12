4376d2344126700c246625a667187dd8119755aa
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-03-12 19:13:42 -0300

    style: enhance CustomTitle component with improved layout and styling

--- src/components/CustomTitle.tsx
+++ src/components/CustomTitle.tsx
@@ -2,12 +2,15 @@ interface CustomTitleProps {
   title: string;
   icon: React.ReactNode;
 }
+
 export const CustomTitle = ({ title, icon }: CustomTitleProps) => {
   return (
-    <div className="my-20 border-b border-slate-600 ">
-      <h2 className="flex items-center mb-2 text-3xl font-semibold gap-x-3 text-black/80 dark:text-white">
-        {icon}
-        {title}
+    <div className="my-20 py-3 border-b border-slate-600 dark:border-slate-400">
+      <h2 className="flex items-center text-3xl gap-x-4 text-gray-900 dark:text-gray-100">
+        <span className="p-2 rounded-full bg-neutral-200/50 text-black dark:bg-neutral-800 dark:text-white shadow-md ">
+          {icon}
+        </span>
+        <span className="font-bold">{title}</span>
       </h2>
     </div>
   );
