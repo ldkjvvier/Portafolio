9a17c5e499699f20147bf4f77ba52590a1940fcc
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-04-07 17:48:14 -0400

    feat: add ABOUT_ME constant with personal information and links

---
+++ src/constants/AboutMeData.ts
@@ -0,0 +1,14 @@
+export const ABOUT_ME = {
+  name: 'Javier Madariaga',
+  title: 'FullStack Developer',
+  description:
+    'Analista Programador de Inacap con {experience} años de experiencia en el desarrollo de aplicaciones web y móviles, con conocimientos en React, NodeJS, MongoDB, MySQL, Firebase, Git, entre otros.',
+  experienceStartYear: 2022,
+  links: {
+    github: 'https://www.github.com/ldkjvvier',
+    linkedin: 'https://www.linkedin.com/in/javiermadariaga/',
+    email: 'javier.a.mada@gmail.com',
+    cv: 'https://drive.google.com/file/d/17oFnIEoHa-3AuWYAxN0y33tL_FlVoHGn/view'
+  },
+  splineScene: 'https://prod.spline.design/Evj8fK2J5nOkiW9l/scene.splinecode'
+};
