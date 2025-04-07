93036369fff527b4851858b58ce94e6c5dc6ac02
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-04-07 01:51:16 -0400

    fix: update dark mode background color in Footer and index.html

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
-	const date = new Date()
-	const year = date.getFullYear()
-	return (
-		<footer className="border-t border-gray-200 transition-all dark:border-gray-500 text-center bg-white dark:bg-background p-5 text-black dark:text-white font-medium">
-			<p>© {year} Javier Madariaga</p>
-		</footer>
-	)
-}
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer className="border-t border-gray-200 transition-all dark:border-gray-500 text-center bg-white dark:bg-[#131221] p-5 text-black dark:text-white font-medium">
+      <p>© {year} Javier Madariaga</p>
+    </footer>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { Home } from './pages/Home/Home';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <Home />
-  </React.StrictMode>
+  </StrictMode>
 );
