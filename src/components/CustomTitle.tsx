9d20e81eb9d6838d7181f6856cd2e2129047c1e1
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-04-07 17:50:16 -0400

    refactor: update CustomTitle component styles for improved theme consistency

--- src/components/CustomTitle.tsx
+++ src/components/CustomTitle.tsx
@@ -6,10 +6,8 @@ interface CustomTitleProps {
 export const CustomTitle = ({ title, icon }: CustomTitleProps) => {
   return (
     <div className="my-20 py-3 border-b border-slate-600 dark:border-slate-400">
-      <h2 className="flex items-center text-3xl gap-x-4 text-gray-900 dark:text-gray-100">
-        <span className="p-2 rounded-full bg-neutral-200/50 text-black dark:bg-neutral-800 dark:text-white shadow-md ">
-          {icon}
-        </span>
+      <h2 className="flex items-center text-3xl gap-x-4 text-theme-primary-light dark:text-theme-primary-dark">
+        <span className="p-2 rounded-full bg-neutral-200/50 dark:bg-neutral-800 shadow-md ">{icon}</span>
         <span className="font-bold">{title}</span>
       </h2>
     </div>
